dbcb2ac7cb418025fccabe4f0721008c11cac685
PrasadPonnada <PrasadPonnada09@gmail.com>
2023-08-02 16:06:42 +0530

    first commit

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'c1u6jvmw',
+    dataset: 'production'
+  }
+})
---
+++ schemas/index.js
@@ -0,0 +1,7 @@
+import comment from './comment'
+import pin from './pin'
+import postedBy from './postedBy'
+import save from './save'
+import user from './user'
+
+export const schemaTypes = [user, pin, comment, postedBy, save]
